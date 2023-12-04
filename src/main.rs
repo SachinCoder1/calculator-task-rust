981ecf992e826d7ca8d0721aa66cb6b01e15b0bc
Sachin Gurjar <98685642+SachinCoder1@users.noreply.github.com>
2023-12-04 12:38:10 +0530

    calculator function with pattern matching

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,23 @@ enum Operation {
 }
 
 
+fn calculate(op: Operation) -> f64 {
+    match op {
+        Operation::Add(a, b) => a + b,
+        Operation::Subtract(a, b) => a - b,
+        Operation::Multiply(a, b) => a * b,
+        Operation::Divide(a, b) => {
+            if b == 0.0 {
+                println!("Cannot divide by zero.");
+                0.0
+            } else {
+                a / b
+            }
+        },
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
